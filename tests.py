d4a69e5d7a38d87be6b0c355bdb0e1ab59fbee62
felipegonzaleze <fgonech@gmail.com>
2025-08-20 05:03:19 -0400

    test: add unit tests for write_file

--- tests.py
+++ tests.py
@@ -1,44 +1,14 @@
-from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
-    
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
-    
-
-    result = get_file_content("calculator", "lorem.txt")
-    print("Result for 'lorem.txt':")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
-    result = get_file_content("calculator", "main.py")
-    print("Result for 'main.py':")
-    print(result)
-
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py':")
-    print(result)
-
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat':")
-    print(result)
-
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print("Result for 'pkg/does_not_exist.py':")
-    print(result)
 if __name__ == "__main__":
     test()
