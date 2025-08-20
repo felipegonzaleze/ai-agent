0efcb9b37d8c0c4e279619ea487f8ad7b38f771d
felipegonzaleze <fgonech@gmail.com>
2025-08-20 03:41:03 -0400

    feat: add unit tests for get_files_info

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 from functions.get_files_info import get_files_info
-
+from functions.get_file_content import get_file_content
 
 def test():
+    """
     result = get_files_info("calculator", ".")
     print("Result for current directory:")
     print(result)
@@ -18,7 +19,21 @@ def test():
     result = get_files_info("calculator", "../")
     print("Result for '../' directory:")
     print(result)
+    """
+    result = get_file_content("calculator", "main.py")
+    print("Result for 'main.py':")
+    print(result)
 
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for 'pkg/calculator.py':")
+    print(result)
 
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for '/bin/cat':")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result for 'pkg/does_not_exist.py':")
+    print(result)
 if __name__ == "__main__":
     test()
