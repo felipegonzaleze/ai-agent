adb087298a72d629e8f2a6dcce7ea4fdc2595190
felipegonzaleze <fgonech@gmail.com>
2025-08-19 23:16:33 -0400

    feat: update main to accept a cli argument for the prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -12,7 +13,7 @@ def main():
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model="gemini-2.0-flash-001", contents=f"{sys.argv[1]}"
     )
 
     print(response.text)
