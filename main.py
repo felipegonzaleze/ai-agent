314e199e7e6e306c6dde7ee558377b753cba9c4f
felipegonzaleze <fgonech@gmail.com>
2025-08-19 23:30:26 -0400

    feat: add '--verbose' flag

--- main.py
+++ main.py
@@ -21,9 +21,13 @@ def main():
         model="gemini-2.0-flash-001", contents=messages
     )
 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"Response: response.text")
+        print(f"User prompt: {sys.argv[1]}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
