bb9a7a364c041baab3aa8d7ffa61e26d4bdc5b94
felipegonzaleze <fgonech@gmail.com>
2025-08-20 03:43:02 -0400

    test: add unit tests for get_files_content

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 
 def test():
-    """
+    
     result = get_files_info("calculator", ".")
     print("Result for current directory:")
     print(result)
@@ -19,7 +19,12 @@ def test():
     result = get_files_info("calculator", "../")
     print("Result for '../' directory:")
     print(result)
-    """
+    
+
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for 'lorem.txt':")
+    print(result)
+
     result = get_file_content("calculator", "main.py")
     print("Result for 'main.py':")
     print(result)
