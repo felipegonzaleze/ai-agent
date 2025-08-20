f08b8838161a137a930f05f86173efdfbd132c50
felipegonzaleze <fgonech@gmail.com>
2025-08-20 03:43:32 -0400

    feat: add get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+from config.config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_target_file = os.path.abspath(full_path)
+
+    if not abs_target_file.startswith(abs_working_dir): return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_target_file): return f'Error: "{file_path}" is not a directory'
+
+    try:
+        with open(abs_target_file, "r") as f:
+            file_content_string = f.read()
+            if len(file_content_string) > MAX_CHARS:
+                file_content_string = file_content_string[:MAX_CHARS - 1]
+                return file_content_string, f"File '{file_path}' truncated at 10000 characters"
+            return file_content_string
+    except Exception as e:
+        return f"Error reading file {e}"
+    
