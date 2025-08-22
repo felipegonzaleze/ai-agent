9c1471cdfae6b89ea08ac90bd07b490921a851f2
felipegonzaleze <fgonech@gmail.com>
2025-08-22 01:13:44 -0400

    feat: add WORKING_DIR const to call_function

--- functions/call_function.py
+++ functions/call_function.py
@@ -3,6 +3,7 @@ from functions.get_file_content import get_file_content, schema_get_file_content
 from functions.get_files_info import get_files_info, schema_get_files_info
 from functions.run_python_file import run_python_file, schema_run_python_file
 from functions.write_file import write_file, schema_write_file
+from config.config import WORKING_DIR
 
 available_functions = types.Tool(
     function_declarations=[
@@ -25,7 +26,7 @@ def call_function(function_name, verbose=False):
     }
 
     kwargs = dict(function_name.args)
-    kwargs['working_directory'] = './calculator'
+    kwargs['working_directory'] = WORKING_DIR
 
     if function_name.name not in functions_dictionary:
         return types.Content(
