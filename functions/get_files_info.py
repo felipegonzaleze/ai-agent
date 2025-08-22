4e866e4fa44d2858ad00fa39b5fc72b36f406a9a
felipegonzaleze <fgonech@gmail.com>
2025-08-21 20:49:35 -0400

    fix: fix schema_get_files_info scope

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -10,7 +10,18 @@ def get_files_info(working_directory, directory="."):
     if not abs_target_dir.startswith(abs_working_dir): return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(abs_target_dir): return f'Error: "{directory}" is not a directory'
 
-    schema_get_files_info = types.FunctionDeclaration(
+    try:
+        files_info = []
+        for file in os.listdir(abs_target_dir):
+            filepath = os.path.join(abs_target_dir, file)
+            file_size = os.path.getsize(filepath)
+            file_is_dir = os.path.isdir(filepath)
+            files_info.append(f"- {file}: file_size={file_size} bytes, is_dir={file_is_dir}")
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error listing files{e}"
+    
+schema_get_files_info = types.FunctionDeclaration(
         name="get_files_info",
         description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
         parameters=types.Schema(
@@ -24,14 +35,3 @@ def get_files_info(working_directory, directory="."):
         )
 
     )
-
-    try:
-        files_info = []
-        for file in os.listdir(abs_target_dir):
-            filepath = os.path.join(abs_target_dir, file)
-            file_size = os.path.getsize(filepath)
-            file_is_dir = os.path.isdir(filepath)
-            files_info.append(f"- {file}: file_size={file_size} bytes, is_dir={file_is_dir}")
-        return "\n".join(files_info)
-    except Exception as e:
-        return f"Error listing files{e}"
