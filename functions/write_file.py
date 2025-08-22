5279dc3fbc5d8b9b5f4b76a5c829b8e56cacd118
felipegonzaleze <fgonech@gmail.com>
2025-08-21 21:29:18 -0400

    feat: add schema of write_file

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def write_file(working_directory, file_path, content):
 
@@ -21,3 +22,21 @@ def write_file(working_directory, file_path, content):
             return f'Successfully wrote to "{file_path} ({len(content)} characters written)'
         except Exception as e:
             return f"Error writing content to file: {e}"
+        
+schema_write_file = types.FunctionDeclaration(
+        name="write_file",
+        description="Write or overwrite files.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "file_path": types.Schema(
+                    type=types.Type.STRING,
+                    description="The path to the file content, relative to the working directory."
+                ),
+                "content": types.Schema(
+                    type=types.Type.STRING,
+                    description="Content to write or overwrite in the specified file path"
+                )
+            }
+        )
+    ) 
