d40c2397e774702122a69fd4aa0654a3a5d5f0d7
felipegonzaleze <fgonech@gmail.com>
2025-08-22 01:11:27 -0400

    feat: create available_functions var

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,10 +1,18 @@
 from google.genai import types
-from functions.get_file_content import get_file_content
-from functions.get_files_info import get_files_info
-from functions.run_python_file import run_python_file
-from functions.write_file import write_file
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
 
-# function_name = response.function_calls
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+    ]
+)
+
 def call_function(function_name, verbose=False):
     
     print(f"Calling function: {function_name.name}({function_name.args})") if verbose else print(f" - Calling function: {function_name.name}")
