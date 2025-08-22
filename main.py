df917c2000b536af4ec628a4a422756528a866c2
felipegonzaleze <fgonech@gmail.com>
2025-08-21 20:41:19 -0400

    feat: create and pass available functions to the LLM

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from config import system_prompt
 
 def main():
-    system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -14,7 +15,18 @@ def main():
         types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
     ]
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001",contents=messages,config=types.GenerateContentConfig(system_instruction=system_prompt))
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt, tools=[available_functions]
+            ))
 
     if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
         print(f"Response: response.text")
@@ -27,3 +39,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Desgaste hueso | algo stock
